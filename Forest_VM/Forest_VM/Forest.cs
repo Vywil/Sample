bcccdc0f5beb8a8bc532251c4d8380f5cae5f24e
Vywil <117413904+Vywil@users.noreply.github.com>
2022-11-14 19:03:41 -0500

    Add files via upload

---
+++ Forest_VM/Forest_VM/Forest.cs
@@ -0,0 +1,59 @@
+﻿using System;
+namespace Forest_VM
+{
+    class Forest
+    {
+        public string name;
+        private string biome;
+        private string forestlayer;
+        public int age;
+
+        public string Biome
+        {
+            get { return biome; }
+            set
+            {
+                if (value == "Tropical" ||
+                    value == "Temperate" ||
+                    value == "Boreal" ||
+                    value == "Desert" ||
+                    value == "Tundra" ||
+                    value == "Taiga" ||
+                    value == "Grassland" ||
+                    value == "Savanna"
+
+                    )
+                {
+                    biome = value + " it's a correct answer!";
+                }
+                else
+                {
+                    biome = value +  " it's not a type of biome.";
+                }
+
+            }
+        }
+        public string Forestlayer
+        {
+            get { return forestlayer; }
+            set
+            {
+                if(
+                    value == "Emergent" ||
+                    value == "Canopy" ||
+                    value == "Understory"||
+                    value == "Forest Floor"
+                    )
+                {
+                    forestlayer = value + " it's a correct answer!";
+                }
+                else
+                {
+                    forestlayer = value + " it's not a forest layer type.";
+                }
+            }
+
+        }
+    }
+}
+
---
+++ Forest_VM/Forest_VM/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Forest_VM;
+
+namespace BasicClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            Forest f = new Forest();
+
+            Console.WriteLine("Type your name to start");
+            f.name = Console.ReadLine();
+            Console.WriteLine("Hello"+" " + f.name +"! "+"This program will ask you a couple of questions about biomes and forest types. Please make sure to capitalize the first letter of your answer");
+
+
+            Console.WriteLine("Type a major biome type");
+            f.Biome = Console.ReadLine();
+            Console.WriteLine(f.Biome);
+
+            Console.WriteLine("Type a rainforest layer");
+            f.Forestlayer = Console.ReadLine();
+            Console.WriteLine(f.Forestlayer);
+        }
+
+    }
+}
+
+
